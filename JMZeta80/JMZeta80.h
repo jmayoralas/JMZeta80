b83c17fff0bea9fd1725756f43bb7cf5dc82cbd5
José Luis Fernández-Mayoralas <yo@jluis.me>
2018-10-02 17:32:19 +0200

    Initial Commit

---
+++ JMZeta80/JMZeta80.h
@@ -0,0 +1,19 @@
+//
+//  JMZeta80.h
+//  JMZeta80
+//
+//  Created by Jose Luis Fernandez-Mayoralas on 2/10/18.
+//  Copyright © 2018 Jose Luis Fernandez-Mayoralas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for JMZeta80.
+FOUNDATION_EXPORT double JMZeta80VersionNumber;
+
+//! Project version string for JMZeta80.
+FOUNDATION_EXPORT const unsigned char JMZeta80VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JMZeta80/PublicHeader.h>
+
+
